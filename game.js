1977abd620964741925b6cdc8a553988c841eb94
Suraj Ray <157343302+SurazRay@users.noreply.github.com>
2024-01-28 15:55:07 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,117 @@
+//alert("Welcome to the classic game of our childhood.\nBy Suraj Ray\nHave fun !!");
+
+const sign = document.querySelectorAll(".sign"); //1
+
+//Array of Choices for Computer, Variables
+
+const choice = ["Rock", "Paper", "Scissors"];  // 3
+let computer, move;  // 1, 3
+var displayId = document.getElementById("cmpRock");  //6
+let result = document.querySelector("h1");  //7
+let userWin = true;  //4, 7
+let countUser = 0, countComp = 0;  //8, 9
+
+//Score Board for User -- 8
+
+const scoreUser = (win) => {
+    win.innerText = ++countUser;
+};
+
+//Score Board for Computer -- 9
+
+const scoreComp = (win) => {
+    win.innerText = ++countComp;
+};
+
+//Game Result Out -- 7
+
+const gameResult = (userWin) => {
+    if (userWin) {
+        result.innerText = "Congrats! You Won";
+        let win = document.querySelector("#user");
+        scoreUser(win); //8
+    }
+    else {
+        result.innerText = "Oops! You lost"
+        let win = document.querySelector("#computer");
+        scoreComp(win); //9
+    }
+};
+
+//Comparing user and computer -- 4
+
+const compareMove = () => {
+    if (move === computer) {
+        console.log("Game Draw");
+        result.innerText = "Game Draw !";
+    }
+    else {
+        if (move === "Rock") {
+            userWin = computer === "Paper" ? false : true;
+        }
+        else if (move === "Paper") {
+            userWin = computer === "Rock" ? true : false;
+        }
+        else {
+            userWin = computer === "Rock" ? false : true;
+        }
+
+        console.log("You win (Boolean) :", userWin);
+        gameResult(userWin); // 7
+    }
+};
+
+//Computer Choice Display message -- 5
+
+const compMsg = (compChoice) => {
+    let compMessage = document.querySelector("#compMsg");
+    compMessage.innerText = `Computer chose ${compChoice}`;
+    compMessage.classList.add("color2");
+    console.log(compMessage.innerText);
+};
+
+//Computer choice Circle border appears -- 6
+
+const compDisplay = (display) => {
+
+    //console.log("Id of element chosen by Computer is " ,display);
+    //Removing Border of previous move and adding border to current move
+
+    displayId.classList.remove("border");
+    displayId = document.getElementById(display);
+    displayId.classList.add("border");
+};
+
+//Computer chooses -- 3
+
+const comp = () => {
+    computer = choice[Math.floor(Math.random() * 3)];
+
+    compMsg(computer); //5
+    compDisplay("cmp" + computer); //6
+    compareMove(); //4
+};
+
+//Print Message -- 2
+
+const message = (msg) => {
+    let user = document.querySelector("#msg");
+    user.innerText = `You chose ${msg}`;
+    user.classList.add("color");
+    console.log(user.innerText);
+
+    comp(); //3
+};
+
+//Adding eventand Accessing all values of the node -- 1
+
+sign.forEach((hand) => {
+
+    hand.addEventListener("click", () => {
+        move = hand.getAttribute("id");
+
+        message(move); //2
+    });
+
+});
+
